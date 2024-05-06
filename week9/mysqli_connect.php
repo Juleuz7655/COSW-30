ba16b5e070f9e903f88a3cec16a2c946ce7a8358
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-05-05 23:00:20 -0700

    change

--- week9/mysqli_connect.php
+++ week9/mysqli_connect.php
@@ -8,7 +8,7 @@
 <?php
 
 //Connect to the database - use these values if you are using my webserver, just change your db name to your own
-$host = "webdevlearning.org"; // For most, just use 127.0.0.1 to indicate your local host which will work for my webserver
+$host = "ftp.webdevlearning.org"; // For most, just use 127.0.0.1 to indicate your local host which will work for my webserver
 $user = "uaqco3xurnwhr"; //Your database username Does not change
 $pass = "cosw30!LBCC"; // Your database user password
 $db = "dbcnl71nvynpuq"; //Your database name you want to connect to - add your number to the end of this
