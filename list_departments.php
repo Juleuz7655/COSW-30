9888199ad8f9a7b2daf3c3362cf2e120c4a52318
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-05-12 16:56:03 -0700

    Commit

---
+++ list_departments.php
@@ -0,0 +1,37 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>List Departments</title>
+        <style> 
+        td {
+            width: 100px;
+        }
+        thead {
+            font-weight: bold;
+        }
+        .center {
+            text-align: center;
+        }
+        </style>
+    </head>
+    <body>
+        <?php
+        require(mysqli_connect.php);
+        
+        echo "<h1> List of Departments";
+
+        $query = "SELECT * FROM DEPARTMENT ORDER BY department_name";
+        $result = mysqli_query($connection, $query);
+
+        echo "<table><thead><td class ='center'> Department ID<td><td>Department Name</td><td>Number of Employees</td><td>Building Number</td><td>Status</td></thead>";
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td class= 'center'>" . $row['department_id']. "</td><td>" . $row['department_name'] . "</td><td>" . $row['num_of_employees']
+            . "</td><td>" . $row['building _number'] . "</td><td>" . $row['status'] . "</td></tr>";
+        }
+        echo "/table";
+        ?>
+        <p> <a href="list_users.php">List Users</a></p>
+
+    </body>
+</html>
