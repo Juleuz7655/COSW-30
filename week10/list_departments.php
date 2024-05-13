7d9b4f6505ee14fd65da2f0a85dd7ff6ceb150f8
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-05-12 18:38:52 -0700

    llist_departments "added hyperlink"

--- week10/list_departments.php
+++ week10/list_departments.php
@@ -23,14 +23,17 @@
         $query = "SELECT * FROM DEPARTMENTS ORDER BY department_name";
         $result = mysqli_query($connection, $query);
 
-        echo "<table><thead><td class ='center'> Department ID<td><td>Department Name</td><td>Number of Employees</td><td>Building Number</td><td>Status</td></thead>";
+        echo "<table><thead><td class ='center'> Department ID<td><td>Department Name</td><td>Number of Employees</td><td>Building Number</td><td>Status</td>
+        <
+        </thead>";
 
         while ($row = mysqli_fetch_assoc($result)) {
             echo "<tr><td class='center'>" . $row['department_id'] . "</td>" .
                  "<td>" . $row['department_name'] . "</td>" .
                  "<td>" . $row['num_of_employees'] . "</td>" .
                  "<td>" . $row['building_number'] . "</td>" .
-                 "<td>" . $row['status'] . "</td></tr>";
+                 "<td>" . $row['status'] . "</td>" . 
+                 "<td> <a href='edit_department.php?id=" . $row['department_id'] . "'>edit</a></td></tr>";
         }
         echo "</table>"; // close table
         ?>
