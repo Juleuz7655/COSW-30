2f300a61ad0e9d10bfa9fe07d420b7b9924c890a
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-05-22 13:00:36 -0700

    Initial Final Test

---
+++ final/index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Star Wars Space Transit Network</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <header>
+            <h1>Space Transit Network</h1>
+            <nav>
+                <ul>
+                    <li><a href="planets.php">Planets</a></li>
+                    <li><a href="users.php">Users</a></li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
