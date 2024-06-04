fdd25dcd73eddcf21bcc81a6398c0c8ccba10584
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-06-04 01:29:07 -0700

    added includes as well as preparing the login funciton

--- final/index.php
+++ final/index.php
@@ -1,25 +1,5 @@
-<html>
-    <head>
-        <title>Star Wars Space Transit Network</title>
-        <link rel="stylesheet" href="css/styles.css">
-    </head>
-    <body>
-        <header>
-            <h1>Space Transit Network</h1>
-            <nav>
-                <ul>
-                    <li><a href="planets.php">Planets</a></li>
-                    <li><a href="login/login.html">Login/Sign Up</a></li>
-                    <li><a href="users/user_list.php">Users</a></li>
-                    <?php if ($role === 'Admin'): ?>
-                        <li><a href="list_planets.php">Planet list</a></li>
-                        <li><a href="list_user.php">User list</a></li>
-                    <?php endif; ?>
-                </ul>
-            </nav>
-        </header>
+<?php include('inlcudes/header.html')?>
         <main>
             <h2><strong>Please login so the Galactic Empire can ensure your safety</strong></h2>
         </main>
-    </body>
-</html>
+<?php include('inlcudes/footer.html')?>
