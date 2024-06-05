dce90f2f17e5014ff32cd756ec480d12694a23be
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-06-05 00:58:02 -0700

    changed table

---
+++ finals_orginizer/user_list.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>List Users</title>
+<link rel="stylesheet" href="css/list_user.css">
+</head>
+<body>
+<!--    <header>
+        <?php// include 'nav.php'; ?>
+    </header>
+-->
+<?php 
+require('../mysqli_connect.php'); // use require because we want to force this to exist before running our queries
+
+echo "<h1>List of Website Users</h1>";
+//And now to perform a simple query to make sure it's working
+$query_neutral = "SELECT * FROM FINAL_USER WHERE TEAM = 'neutral'";
+$result_neutral = mysqli_query($connection, $query_neutral);
+
+echo "<h2>Neutral Users</h2>";
+echo "<table><thead><td class='center'>ID</td><td>First Name</td><td>Last Name</td><td>Email Address</td><td>Role</td><td>Status</td><td>Action</td><td>Team</td></thead>"; // open table and include table headings
+
+while ($row_neutral = mysqli_fetch_assoc($result_neutral)) {
+echo "<tr><td class='center'>" . $row_neutral['user_id'] . "</td><td>" . $row_neutral['first_name'] . "</td><td>" . $row_neutral['last_name'] . "</td><td>" . $row_neutral['email_address'] . "</td><td>" . $row_neutral['role'] . "</td><td>" . "</td><td>" . $row_neutral['status'] . "</td><td>" . $row_neutral['team'] . "</td><td><a href='user.php?id=" . $row_neutral['user_id'] . "'>View</a> / <a href='edit_user.php?id=" . $row_neutral['user_id'] . "'>Edit</a></td></tr>";
+}
+echo "</table>"; // close table
+
+
+$query_light = "SELECT * FROM FINAL_USER WHERE TEAM = 'light'";
+$result_light = mysqli_query($connection, $query_light);
+
+echo "<h2>Light Users</h2>";
+echo "<table><thead><td class='center'>ID</td><td>First Name</td><td>Last Name</td><td>Email Address</td><td>Role</td><td>Status</td><td>Action</td><td>Team</td></thead>"; // open table and include table headings
+
+while ($row_light = mysqli_fetch_assoc($result_light)) {
+echo "<tr><td class='center'>" . $row_light['user_id'] . "</td><td>" . $row_light['first_name'] . "</td><td>" . $row_light['last_name'] . "</td><td>" . $row_light['email_address'] . "</td><td>" . $row_light['role'] . "</td><td>" . $row_light['status'] . "</td><td>" . "</td><td>" . $row_light['team'] . "</td><td><a href='user.php?id=" . $row_light['user_id'] . "'>View</a> / <a href='edit_user.php?id=" . $row_light['user_id'] . "'>Edit</a></td></tr>";
+}
+echo "</table>"; // close table
+
+$query_dark = "SELECT * FROM FINAL_USER WHERE TEAM = 'dark'";
+$result_dark = mysqli_query($connection, $query_dark);
+
+echo "<h2>Dark Users</h2>";
+echo "<table><thead><td class='center'>ID</td><td>First Name</td><td>Last Name</td><td>Email Address</td><td>Role</td><td>Status</td><td>Action</td><td>Team</td></thead>"; // open table and include table headings
+
+while ($row_dark = mysqli_fetch_assoc($result_dark)) {
+echo "<tr><td class='center'>" . $row_dark['user_id'] . "</td><td>" . $row_dark['first_name'] . "</td><td>" . $row_dark['last_name'] . "</td><td>" . $row_dark['email_address'] . "</td><td>" . $row_dark['role'] . "</td><td>" . $row_dark['status'] . "</td><td>" . $row_dark['team'] . "</td><td><a href='user.php?id=" . $row_dark['user_id'] . "'>View</a> / <a href='edit_user.php?id=" . $row_dark['user_id'] . "'>Edit</a></td></tr>";
+}
+echo "</table>"; // close table
+
+?>
+</body>
+</html>
