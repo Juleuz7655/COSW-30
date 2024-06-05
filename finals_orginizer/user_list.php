460868c4fee89fe7d699257e4593ef583381ac05
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-06-05 01:14:32 -0700

    fixed connection

--- finals_orginizer/user_list.php
+++ finals_orginizer/user_list.php
@@ -21,7 +21,7 @@ text-align:center;
     </header>
 -->
 <?php 
-require('../mysqli_connect.php'); // use require because we want to force this to exist before running our queries
+require('mysqli_connect.php'); // use require because we want to force this to exist before running our queries
 
 echo "<h1>List of Website Users</h1>";
 //And now to perform a simple query to make sure it's working
