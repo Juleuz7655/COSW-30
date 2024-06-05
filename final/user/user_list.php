3895c2851e2addbf0f67f441d785ad7673822ed5
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-06-05 14:02:03 -0700

    fixed mysqli_connect placemenet

--- final/user/user_list.php
+++ final/user/user_list.php
@@ -5,13 +5,16 @@
 <link rel="stylesheet" href="../css/user_list.css">
 
 </head>
+
+<?php require('../mysqli_connect.php');?>
+
 <body>
 <!--    <header>
         <?php('../includes/navigation.html') ?>
     </header>
 -->
 <?php 
-require('../mysqli_connect.php'); // use require because we want to force this to exist before running our queries
+ // use require because we want to force this to exist before running our queries
 
 echo "<h1>List of Website Users</h1>";
 //And now to perform a simple query to make sure it's working
