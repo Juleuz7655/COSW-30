b019c2558a7305d9292d3492be66189fdf34e400
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-06-05 05:27:06 -0700

    made a view users for regular users

---
+++ final/user/reg/view_user.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Star Wars Space Transit Center</title>
+<link href="reg.css" rel="stylesheet">
+</head>
+<body>
+<h1>Choose a side</h1>
+
+<div class="outer-flex-container">
+    <h2> Light Side</h2>
+    <div class="flex-group">
+        <div class="flex-item">Item 1</div>
+        <div class="flex-item">Item 2</div>
+        <div class="flex-item">Item 3</div>
+    </div>
+
+    <h2> Neutral</h2>
+    <div class="flex-group">
+        <div class="flex-item">Item 4</div>
+        <div class="flex-item">Item 5</div>
+        <div class="flex-item">Item 6</div>
+    </div>
+
+    <h2> Dark Side</h2>
+    <div class="flex-group">
+        <div class="flex-item">Item 7</div>
+        <div class="flex-item">Item 8</div>
+        <div class="flex-item">Item 9</div>
+    </div>
+</div>
