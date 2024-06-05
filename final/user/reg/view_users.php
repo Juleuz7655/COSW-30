12353215574398471efdde375201243aaef375a7
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-06-05 14:05:26 -0700

    renamed containers

--- final/user/reg/view_users.php
+++ final/user/reg/view_users.php
@@ -14,23 +14,23 @@
 <div class="container">
     <h2> Light Side</h2>
     <div class="group">
-        <div class="flex-item">Item 1</div>
-        <div class="flex-item">Item 2</div>
-        <div class="flex-item">Item 3</div>
+        <div class="item">Item 1</div>
+        <div class="item">Item 2</div>
+        <div class="item">Item 3</div>
     </div>
 
     <h2> Neutral</h2>
     <div class="group">
-        <div class="flex-item">Item 4</div>
-        <div class="flex-item">Item 5</div>
-        <div class="flex-item">Item 6</div>
+        <div class="item">Item 4</div>
+        <div class="item">Item 5</div>
+        <div class="item">Item 6</div>
     </div>
 
     <h2> Dark Side</h2>
     <div class="group">
-        <div class="flex-item">Item 7</div>
-        <div class="flex-item">Item 8</div>
-        <div class="flex-item">Item 9</div>
+        <div class="item">Item 7</div>
+        <div class="item">Item 8</div>
+        <div class="item">Item 9</div>
     </div>
 </div>
 
