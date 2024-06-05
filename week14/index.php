856e25e7c98166917c006c27ee3d1f3d764eb737
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-06-05 15:40:57 -0700

    added session start

--- week14/index.php
+++ week14/index.php
@@ -1,5 +1,19 @@
 <?php include('includes/header.html');?>
 
-Hello there...
 
+<?php
+// Just to make sure if session_start is active
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+
+
+?>
+
+
+
+<?php
+"<p>Welcome, {$_SESSION['first_name']}!</p>"
+?>
 <?php include('includes/footer.html');?>
