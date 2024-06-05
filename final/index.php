96766bdd398f29bc33fa2e91446b32e1ea990e07
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-06-04 22:25:44 -0700

    changed header

--- final/index.php
+++ final/index.php
@@ -15,28 +15,7 @@ $role = isset($_SESSION['role']) ? $_SESSION['role'] : '';
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <header>
-        <h1>Space Transit Network</h1>
-        <nav>
-            <ul>
-                <li><a href="planets.php">Planets</a></li>
-                <li>
-                    <?php
-                    if (isset($_SESSION['user_id'])) {
-                        echo '<a href="logout.php">Logout</a>';
-                    } else {
-                        echo '<a href="login.php">Login</a>';
-                    }
-                    ?>
-                </li>
-                <li><a href="users/user_list.php">Users</a></li>
-                <?php if ($role === 'Admin'): ?>
-                    <li><a href="list_planets.php">Planet list</a></li>
-                    <li><a href="list_user.php">User list</a></li>
-                <?php endif; ?>
-            </ul>
-        </nav>
-    </header>
+
 
     <main>
         <p>Hello there...</p>
