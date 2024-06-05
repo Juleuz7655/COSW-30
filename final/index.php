4b79b32c557668fd20bdb5cd5c6d900b005c9a88
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-06-04 23:31:36 -0700

    Modified index

--- final/index.php
+++ final/index.php
@@ -7,21 +7,9 @@ $first_name = isset($_SESSION['first_name']) ? $_session['first_name'] : '';
 $role = isset($_SESSION['role']) ? $_SESSION['role'] : '';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Star Wars Space Transit Network</title>
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-
 
     <main>
         <h2></h2>
     </main>
 
     <?php include('includes/footer.html'); ?>
-</body>
-</html>
