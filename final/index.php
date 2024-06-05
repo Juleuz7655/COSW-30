860bab8bcfb2c39d410fc680121ae82617f2962b
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-06-04 23:44:18 -0700

    modified index.php

--- final/index.php
+++ final/index.php
@@ -3,8 +3,19 @@ session_start();
 
 include('includes/header.html');
 
-$first_name = isset($_SESSION['first_name']) ? $_session['first_name'] : '';
-$role = isset($_SESSION['role']) ? $_SESSION['role'] : '';
+if (!isset($_SESSION['role'])) {
+    $_SESSION['role'] = 'User';
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['toggle_role'])) {
+    if ($_SESSION['role'] === 'Admin') {
+        $_SESSION['role'] = 'User';
+    } else {
+        $_SESSION['role'] = 'Admin';
+    }
+}
+
+$role = $_SESSION['role'];
 ?>
 
 
