7a686788997544040464058b3c384eb937bdbddb
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-06-05 01:59:04 -0700

    Added action to user list

--- finals_orginizer/user_list.php
+++ finals_orginizer/user_list.php
@@ -19,7 +19,7 @@ $query_neutral = "SELECT * FROM FINAL_USERS WHERE TEAM = 'Neutral'";
 $result_neutral = mysqli_query($connection, $query_neutral);
 
 echo "<h2>Neutral Users</h2>";
-echo "<table><thead><td class='center'>ID</td><td>First Name</td><td>Last Name</td><td>Email Address</td><td>Role</td><td>Team</td></thead>"; // open table and include table headings
+echo "<table><thead><td class='center'>ID</td><td>First Name</td><td>Last Name</td><td>Email Address</td><td>Role</td><td>Team</td><td>Action</td></thead>"; // open table and include table headings
 
 while ($row_neutral = mysqli_fetch_assoc($result_neutral)) {
 echo "<tr><td class='center'>" . $row_neutral['user_id'] . "</td><td>" . $row_neutral['first_name'] . "</td><td>" . $row_neutral['last_name'] . "</td><td>" . $row_neutral['email_address'] . "</td><td>" . $row_neutral['role'] . "</td><td>" . $row_neutral['team'] . "</td><td><a href='user.php?id=" . $row_neutral['user_id'] . "'>View</a> / <a href='edit_user.php?id=" . $row_neutral['user_id'] . "'>Edit</a></td></tr>";
@@ -31,7 +31,7 @@ $query_light = "SELECT * FROM FINAL_USERS WHERE TEAM = 'Light'";
 $result_light = mysqli_query($connection, $query_light);
 
 echo "<h2>Light Users</h2>";
-echo "<table><thead><td class='center'>ID</td><td>First Name</td><td>Last Name</td><td>Email Address</td><td>Role</td><td>Team</td></thead>"; // open table and include table headings
+echo "<table><thead><td class='center'>ID</td><td>First Name</td><td>Last Name</td><td>Email Address</td><td>Role</td><td>Team</td><td>Action</td></thead>"; // open table and include table headings
 
 while ($row_light = mysqli_fetch_assoc($result_light)) {
 echo "<tr><td class='center'>" . $row_light['user_id'] . "</td><td>" . $row_light['first_name'] . "</td><td>" . $row_light['last_name'] . "</td><td>" . $row_light['email_address'] . "</td><td>" . $row_light['role'] . "</td><td>" . $row_light['team'] . "</td><td><a href='user.php?id=" . $row_light['user_id'] . "'>View</a> / <a href='edit_user.php?id=" . $row_light['user_id'] . "'>Edit</a></td></tr>";
@@ -42,7 +42,7 @@ $query_dark = "SELECT * FROM FINAL_USERS WHERE TEAM = 'Dark'";
 $result_dark = mysqli_query($connection, $query_dark);
 
 echo "<h2>Dark Users</h2>";
-echo "<table><thead><td class='center'>ID</td><td>First Name</td><td>Last Name</td><td>Email Address</td><td>Role</td><td>Team</td></thead>"; // open table and include table headings
+echo "<table><thead><td class='center'>ID</td><td>First Name</td><td>Last Name</td><td>Email Address</td><td>Role</td><td>Team</td><td>Action</td></thead>"; // open table and include table headings
 
 while ($row_dark = mysqli_fetch_assoc($result_dark)) {
 echo "<tr><td class='center'>" . $row_dark['user_id'] . "</td><td>" . $row_dark['first_name'] . "</td><td>" . $row_dark['last_name'] . "</td><td>" . $row_dark['email_address'] . "</td><td>" . $row_dark['role'] . "</td><td>" . $row_dark['team'] . "</td><td><a href='user.php?id=" . $row_dark['user_id'] . "'>View</a> / <a href='edit_user.php?id=" . $row_dark['user_id'] . "'>Edit</a></td></tr>";
