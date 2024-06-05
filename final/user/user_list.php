d23cd975120e38a3a20ce019a5746ff8594ffb73
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-06-05 14:04:28 -0700

    revert2

--- final/user/user_list.php
+++ final/user/user_list.php
@@ -13,7 +13,7 @@
     </header>
 -->
 <?php 
-<?php require('../mysqli_connect.php');?>
+ require('../mysqli_connect.php');
 // use require because we want to force this to exist before running our queries
 
 echo "<h1>List of Website Users</h1>";
