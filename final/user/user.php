3a83716cf5bdd642c2f54a16e3d2a259ce42a9bf
Cheetah7655 <151423784+Juleuz7655@users.noreply.github.com>
2024-06-05 22:20:51 -0700

    added user.php css

--- final/user/user.php
+++ final/user/user.php
@@ -1,36 +1,30 @@
-<?php
-
-$pagetitle = "User Information";
-
-require('../mysqli_connect.php'); // use require because we want to force this to exist before running our queries
-
-if ($_SERVER["REQUEST_METHOD"] == "GET")
-{
-    $user_id = $_GET['id'];
-    $query = "SELECT * FROM FINAL_USERS WHERE user_id = $user_id";
-    $result = mysqli_query($connection, $query);
-    $row = mysqli_fetch_array($result);
-    echo "user ID: " . $user_id . " ";
-    echo $query;
-} else {
-    echo "wasn't me";
-}
-?>
-
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pagetitle; ?></title>
+    <link rel="stylesheet" href="../css/user.css">
 </head>
 <body>
     <header>
         <?php include 'nav.php'; ?>
     </header>
-    <h1><?php echo $pagetitle; ?></h1>
-    <p style="background: url(<?php echo $row['photo']; ?>); background-position: center; background-size: cover; border-radius: 50px; height: 100px; width: 100px">&nbsp;</p>
-    <p><?php echo $row['first_name']  . ' ' . $row['last_name'] . ', ' . $row['role']; ?><br />
-    <?php echo $row['email_address']; ?>, <?php echo $row['team']; ?></p>
-    <p><a href="edit_user.php?id=<?php echo $user_id;?>">Edit this user</a></p>
+    <main>
+        <h1><?php echo $pagetitle; ?></h1>
+        <div class="user-info">
+            <div class="user-photo" style="background-image: url(<?php echo $row['photo']; ?>);">&nbsp;</div>
+            <div class="user-details">
+                <p><?php echo $row['first_name'] . ' ' . $row['last_name']; ?></p>
+                <p>Role: <?php echo $row['role']; ?></p>
+                <p>Email: <?php echo $row['email_address']; ?></p>
+                <p>Team: <?php echo $row['team']; ?></p>
+                <p><a href="edit_user.php?id=<?php echo $user_id;?>">Edit this user</a></p>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <p>&copy; 2024 Star Wars Space Transit Center</p>
+    </footer>
 </body>
 </html>
